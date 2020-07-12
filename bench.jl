82e501ae44ad61b60788b16d55c4f33f53739ca8
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2020-07-12 17:45:58 +0530

    use mean for benchmarking

--- bench.jl
+++ bench.jl
@@ -1,6 +1,7 @@
 using SparseArrays, LightGraphs, BenchmarkTools
 using LightGraphs: insorted
 using LightGraphs.SimpleGraphsCore: SimpleGraph
+using BenchmarkTools: mean
 BenchmarkTools.DEFAULT_PARAMETERS.samples = 100
 
 include("loadtsv.jl")
@@ -14,9 +15,9 @@ A = loadtsv(gpath, SparseMatrixCSC)
 println("|V| = ", nv(SG), " |E| = ", ne(SG))
 
 t = @benchmark triangle_count($SG, DODG())
-println("dodg: ", "number of triangles = ", triangle_count(SG, DODG()), ", time = ", t)
+println("dodg: ", "number of triangles = ", triangle_count(SG, DODG()), ", time = ", mean(t))
 
 t = @benchmark triangle_count($A, MatrixTrace())
-println("matrixtrace: ", "number of triangles = ", triangle_count(A, MatrixTrace()), ", time = ", t)
+println("matrixtrace: ", "number of triangles = ", triangle_count(A, MatrixTrace()), ", time = ", mean(t))
 
 println("=============================================")
