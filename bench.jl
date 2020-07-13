741b10515fddf416260d553d9abef314d53c7555
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2020-07-13 18:35:00 +0530

    bug fix

--- bench.jl
+++ bench.jl
@@ -12,10 +12,10 @@ A = loadtsv(gpath, SparseMatrixCSC)
 
 println("|V| = ", nv(SG), " |E| = ", ne(SG))
 
-println("dodg: ", "number of triangles = ", triangle_count(SG, DODG()), ", time = ", @elapsed triangle_count($SG, DODG()))
+println("dodg: ", "number of triangles = ", triangle_count(SG, DODG()), ", time = ", @elapsed triangle_count(SG, DODG()))
 
-println("threadeddodg: ", "number of triangles = ", triangle_count(SG, ThreadedDODG()), ", time = ", @elapsed triangle_count($SG, ThreadedDODG()))
+println("threadeddodg: ", "number of triangles = ", triangle_count(SG, ThreadedDODG()), ", time = ", @elapsed triangle_count(SG, ThreadedDODG()))
 
-println("matrixtrace: ", "number of triangles = ", triangle_count(A, MatrixTrace()), ", time = ", @elapsed triangle_count($A, MatrixTrace()))
+println("matrixtrace: ", "number of triangles = ", triangle_count(A, MatrixTrace()), ", time = ", @elapsed triangle_count(A, MatrixTrace()))
 
 println("=============================================")
